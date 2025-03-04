6e4238fe1d6e057fc10be269facb2fa3bd7f6248
corey clarke <sombas100@yahoo.com>
2025-03-04 16:55:17 +0000

    Create the offer banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -3,8 +3,17 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="flex items-center justify-center bg-zinc-600 h-16">
-      <div>
-        <button>Banner</button>
+      <div className="flex items-center justify-center gap-3">
+        <button
+          style={{ padding: "5px" }}
+          className="bg-amber-300 hover:cursor-pointer hover:bg-amber-500 transition-all ease-in"
+        >
+          Exclusive offer
+        </button>
+        <span className="text-white font-bold">UP TO 20% OFF</span>
+        <p className="text-zinc-300 text-md">
+          Use CODE Softu | Limited time offer
+        </p>
       </div>
     </div>
   );
