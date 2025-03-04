4d7c3633f1270693827b6823b3458d55286a261c
corey clarke <sombas100@yahoo.com>
2025-03-04 16:33:28 +0000

    Install tailwind

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="flex items-center justify-center bg-zinc-600 h-16">
+      <div>
+        <button>Banner</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
