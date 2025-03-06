b1f34ba5cf8958761a1db23eb22ecf7733adc950
corey clarke <sombas100@yahoo.com>
2025-03-06 03:06:08 +0000

    Add shadow hover effect on feature cards

--- client/src/components/ui/Card.tsx
+++ client/src/components/ui/Card.tsx
@@ -4,7 +4,9 @@ const Card = ({ children }) => {
   return (
     <div
       style={{ paddingBottom: "20px" }}
-      className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+      className="max-w-sm p-6 bg-white border border-gray-200 
+      rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700
+       hover:shadow-xl transition-all ease-in"
     >
       <a href="#">
         <h5
